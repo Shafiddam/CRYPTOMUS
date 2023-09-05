64ba8191971e656ddcd65711b6dd53bed0301e3e
Fidail Shafikov <93327801+Shafiddam@users.noreply.github.com>
2023-09-05 16:11:29 +0300

    Update test_login_email.py

--- test_login_email.py
+++ test_login_email.py
@@ -1,6 +1,4 @@
 import pytest
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 
 from .data.data import passwords
 from .pages.login_page import LoginPage
@@ -9,10 +7,10 @@ from .pages.login_page import LoginPage
 def test_login_email(driver):
     """
     Вход по почте
-    Аккаунт enot_2022_001@mail.ru, для подтверждения просит код 2фа
+    Аккаунт enot_2022_001@mail.ru, для подтверждения просит только 2фа
     """
     login_page = LoginPage(driver)  # Создание объекта LoginPage
-    # Получение пароля из словаря
+    # Получите пароль из словаря
     email = "enot_2022_001@mail.ru"
     password_for_email = passwords.get(email, "DefaultPasswordIfNotFound")
     try:
@@ -23,9 +21,7 @@ def test_login_email(driver):
         code2fa = login_page.code2fa(email)
         login_page.send_keys_2fa(code2fa)
         login_page.click_login_confirm()
-        # Ожидание перехода на новую страницу (ожидаем, пока URL не будет равен указанному)
-        # то есть проверяем, что мы вошли в ЛК
-        WebDriverWait(driver, 10).until(EC.url_to_be('https://app.cryptomus.com/dashboard/'))
+        login_page.compare()  # проверяем что вошли в дашборд
     except Exception as e:
         pytest.fail(f"ERROR: {str(e)}")
     finally:
