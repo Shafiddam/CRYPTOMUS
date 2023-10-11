cf143d523c06facd7eb7b06e75be39b02e8c24cf
Fidail Shafikov <93327801+Shafiddam@users.noreply.github.com>
2023-10-11 12:46:45 +0300

    Add files via upload

---
+++ data/config.py
@@ -0,0 +1,6 @@
+CRYPTO_TO_NETWORKS = {
+    'USDT': ['BSC', 'Tron', 'Polygon', 'ETH'],
+    'BUSD': ['BSC', 'ETH'],
+    'DAI': ['Polygon', 'BSC', 'ETH'],
+    'USDC': ['Polygon', 'Tron', 'BSC', 'ETH']
+}
