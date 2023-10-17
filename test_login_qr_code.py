16183bffacc70c12d0639af826d9f1316efe0e69
Fidail Shafikov <93327801+Shafiddam@users.noreply.github.com>
2023-10-17 12:10:29 +0300

    Update test_login_qr_code.py

--- test_login_qr_code.py
+++ test_login_qr_code.py
@@ -5,12 +5,11 @@ from .pages.login_page import LoginPage
 
 def test_login_qr_code(driver):
     """ Ожид.результат: кнопка есть, кликается, появляется модалка "Log in with QR code" """
-    login_page = LoginPage(driver)  # Создание объекта LoginPage
+    login_page = LoginPage(driver)  
     try:
         login_page.open()
         login_page.click_login_qr_code()
         modal = login_page.modal_qr_code()  # поиск модального окна
-        # Проверка, что модалка появилась:
         assert modal.is_displayed(), "ОШИБКА: модалки нет !!!"
 
         qr_code = login_page.qr_code_pict()  # поиск картинки QR-кода
@@ -25,9 +24,8 @@ def test_login_qr_code(driver):
         data = login_page.qr_code_screenshot_decode(screenshot_path)
         # проверка, что QR-код "https://app.cryptomus.com/dashboard?token=" найден на скриншоте
         assert "https://app.cryptomus.com/dashboard?token=" in data, "ОШИБКА: QR-код не распознан!"
+        
     except Exception as e:
-        # обработка исключений
         pytest.fail(f"ERROR: {str(e)}")
     finally:
-        # закрытие браузера в любом случае
         driver.quit()
