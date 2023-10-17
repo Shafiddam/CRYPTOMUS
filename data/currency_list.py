e90ea45b04e2bca703885ad961bbc02c15b18f23
Fidail Shafikov <93327801+Shafiddam@users.noreply.github.com>
2023-10-17 12:01:09 +0300

    Add files via upload

---
+++ data/currency_list.py
@@ -0,0 +1,15 @@
+cripto_list = ['AVAX', 'BCH', 'BNB', 'BTC', 'BUSD', 'CGPT', 'DAI', 'DASH', 'DOGE', 'ETH',  # 10
+               'LTC', 'MATIC', 'SOL', 'TON', 'TRX', 'USDC', 'USDT', 'VERSE', 'XMR']  # 9
+
+# cripto_list = ['CGPT']
+
+fiat_list = ['AMD', 'AZN', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'EUR', 'GBP', 'IDR',  # 10
+             'INR', 'ISK', 'JPY', 'KRW', 'KZT', 'MYR', 'NOK', 'PHP', 'PLN', 'RUB',  # 10
+             'SEK', 'THB', 'TMT', 'TRY', 'UAH', 'USD', 'UZS', 'VND']  # 8
+
+
+# def test_sorted_list():
+#     sorted_cripto_list = sorted(cripto_list)
+#     sorted_fiat_list = sorted(fiat_list)
+#     print('\nsorted_cripto_list = ',sorted_cripto_list)
+#     print('\nsorted_fiat_list = ',sorted_fiat_list)
