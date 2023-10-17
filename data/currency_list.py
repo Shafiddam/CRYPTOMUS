a03d919f320c8d851745ee289b91b4bb8326922e
Fidail Shafikov <93327801+Shafiddam@users.noreply.github.com>
2023-10-17 18:58:50 +0300

    Add files via upload

--- data/currency_list.py
+++ data/currency_list.py
@@ -1,12 +1,14 @@
-cripto_list = ['AVAX', 'BCH', 'BNB', 'BTC', 'BUSD', 'CGPT', 'DAI', 'DASH', 'DOGE', 'ETH',  
-               'LTC', 'MATIC', 'SOL', 'TON', 'TRX', 'USDC', 'USDT', 'VERSE', 'XMR']  
+cripto_list = ['AVAX', 'BCH', 'BNB', 'BTC', 'BUSD', 'CGPT', 'DAI', 'DASH', 'DOGE', 'ETH',  # 10
+               'LTC', 'MATIC', 'SOL', 'TON', 'TRX', 'USDC', 'USDT', 'VERSE', 'XMR']  # 9
 
 # cripto_list = ['CGPT']
 
-fiat_list = ['AMD', 'AZN', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'EUR', 'GBP', 'IDR',  
-             'INR', 'ISK', 'JPY', 'KRW', 'KZT', 'MYR', 'NOK', 'PHP', 'PLN', 'RUB',  
-             'SEK', 'THB', 'TMT', 'TRY', 'UAH', 'USD', 'UZS', 'VND']  
+fiat_list = ['AMD', 'AZN', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'EUR', 'GBP', 'IDR',  # 10
+             'INR', 'ISK', 'JPY', 'KRW', 'KZT', 'MYR', 'NOK', 'PHP', 'PLN', 'RUB',  # 10
+             'SEK', 'THB', 'TMT', 'TRY', 'UAH', 'USD', 'UZS', 'VND']  # 8
 
+cripto_list_with_one_network = ['AVAX', 'BCH', 'BNB', 'BTC', 'CGPT', 'DASH', 'DOGE', 'LTC', 'SOL', 'TON',  # 10
+                                'TRX', 'VERSE', 'XMR']  # 3
 
 # def test_sorted_list():
 #     sorted_cripto_list = sorted(cripto_list)
