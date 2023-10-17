6d5ead0dfc85f54ff7aea1ebf7beeed904de5e5a
Fidail Shafikov <93327801+Shafiddam@users.noreply.github.com>
2023-10-17 12:13:06 +0300

    Update test_login_telegram.py

--- test_login_telegram.py
+++ test_login_telegram.py
@@ -5,7 +5,7 @@ from .pages.login_page import LoginPage
 def test_login_telegram(driver):
     """ Ожид.результат: кнопка есть, кликается, появляется новое окно "Telegram Authorization"
     и ссылка вида https://oauth.telegram.org/auth?bot_id..."""
-    login_page = LoginPage(driver)  # Создание объекта LoginPage
+    login_page = LoginPage(driver)   
     try:
         login_page.open()
         login_page.click_login_telegram()
@@ -17,8 +17,8 @@ def test_login_telegram(driver):
         assert driver.title == "Telegram Authorization"
         assert driver.current_url == \
         "https://oauth.telegram.org/auth?bot_id=5746172286&origin=https%3A%2F%2Fapp.cryptomus.com&embed=1&request_access=write&return_to=https%3A%2F%2Fapp.cryptomus.com%2Flogin"
+        
     except Exception as e:
-        # обработка исключений
         pytest.fail(f"ERROR: {str(e)}")
     finally:
         driver.quit()
