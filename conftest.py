e00912ea9dc34284a61b61e3f71d443b8fbdae8d
Fidail Shafikov <93327801+Shafiddam@users.noreply.github.com>
2023-10-21 21:36:20 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -14,11 +14,12 @@ def driver():
     chrome_options = Options()
     chrome_options.add_argument("--lang=en-US")  # Установите желаемый язык, например, en-US для английского
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--log-level=DEBUG")
     #service = Service(os.environ.get("CHROMEWEBDRIVER"))
     #driver = webdriver.Chrome(service=service, options=chrome_options)
-    driver = webdriver.Chrome(options=chrome_options)
-    #chromedriver_path = os.environ.get("CHROMEWEBDRIVER")
-    #driver = webdriver.Chrome(executable_path=chromedriver_path, options=chrome_options)
+    #driver = webdriver.Chrome(options=chrome_options)
+    chromedriver_path = os.environ.get("CHROMEWEBDRIVER")
+    driver = webdriver.Chrome(executable_path=chromedriver_path, options=chrome_options)
     #driver.maximize_window()  
     yield driver
     driver.quit()
@@ -30,46 +31,46 @@ def login_page(driver, link_prod='https://app.cryptomus.com/login'):
     yield page
 
 
-@pytest.fixture
-def login_and_prepare():
-    chrome_options = Options()
-    chrome_options.add_argument("--lang=en-US")  # Установите желаемый язык, например, en-US для английского
-    chrome_options.add_argument("--headless")
-    #service = Service(os.environ.get("CHROMEWEBDRIVER"))
-    #driver = webdriver.Chrome(service=service, options=chrome_options)
-    driver = webdriver.Chrome(options=chrome_options)
-    #chromedriver_path = os.environ.get("CHROMEWEBDRIVER")
-    #driver = webdriver.Chrome(executable_path=chromedriver_path, options=chrome_options)
-    #driver.maximize_window()  
-    login_page = LoginPage(driver)
-    dashboard_page = DashboardPage(driver)
-    email = "enot_2022_001@mail.ru"
-    password_for_email = passwords.get(email, "DefaultPasswordIfNotFound")
-    login_page.login_with_2fa(email, password_for_email)
-    dashboard_page.confirm_cookies()
-    yield driver, dashboard_page
-    driver.quit()
+# @pytest.fixture
+# def login_and_prepare():
+#     chrome_options = Options()
+#     chrome_options.add_argument("--lang=en-US")  # Установите желаемый язык, например, en-US для английского
+#     chrome_options.add_argument("--headless")
+#     #service = Service(os.environ.get("CHROMEWEBDRIVER"))
+#     #driver = webdriver.Chrome(service=service, options=chrome_options)
+#     driver = webdriver.Chrome(options=chrome_options)
+#     #chromedriver_path = os.environ.get("CHROMEWEBDRIVER")
+#     #driver = webdriver.Chrome(executable_path=chromedriver_path, options=chrome_options)
+#     #driver.maximize_window()  
+#     login_page = LoginPage(driver)
+#     dashboard_page = DashboardPage(driver)
+#     email = "enot_2022_001@mail.ru"
+#     password_for_email = passwords.get(email, "DefaultPasswordIfNotFound")
+#     login_page.login_with_2fa(email, password_for_email)
+#     dashboard_page.confirm_cookies()
+#     yield driver, dashboard_page
+#     driver.quit()
 
 
-@pytest.fixture
-def login_and_prepare_settings():
-    chrome_options = Options()
-    chrome_options.add_argument("--lang=en-US")  # Установите желаемый язык, например, en-US для английского
-    chrome_options.add_argument("--headless")
-    #service = Service(os.environ.get("CHROMEWEBDRIVER"))
-    #driver = webdriver.Chrome(service=service, options=chrome_options)
-    driver = webdriver.Chrome(options=chrome_options)
-    #chromedriver_path = os.environ.get("CHROMEWEBDRIVER")
-    #driver = webdriver.Chrome(executable_path=chromedriver_path, options=chrome_options)
-    #driver.maximize_window()  
-    login_page = LoginPage(driver)
-    dashboard_page = DashboardPage(driver)
-    email = "enot_2022_001@mail.ru"
-    password_for_email = passwords.get(email, "DefaultPasswordIfNotFound")
-    first_code2fa = login_page.login_with_2fa(email, password_for_email)
-    dashboard_page.confirm_cookies()
-    yield driver, dashboard_page, first_code2fa
-    driver.quit()
+# @pytest.fixture
+# def login_and_prepare_settings():
+#     chrome_options = Options()
+#     chrome_options.add_argument("--lang=en-US")  # Установите желаемый язык, например, en-US для английского
+#     chrome_options.add_argument("--headless")
+#     #service = Service(os.environ.get("CHROMEWEBDRIVER"))
+#     #driver = webdriver.Chrome(service=service, options=chrome_options)
+#     driver = webdriver.Chrome(options=chrome_options)
+#     #chromedriver_path = os.environ.get("CHROMEWEBDRIVER")
+#     #driver = webdriver.Chrome(executable_path=chromedriver_path, options=chrome_options)
+#     #driver.maximize_window()  
+#     login_page = LoginPage(driver)
+#     dashboard_page = DashboardPage(driver)
+#     email = "enot_2022_001@mail.ru"
+#     password_for_email = passwords.get(email, "DefaultPasswordIfNotFound")
+#     first_code2fa = login_page.login_with_2fa(email, password_for_email)
+#     dashboard_page.confirm_cookies()
+#     yield driver, dashboard_page, first_code2fa
+#     driver.quit()
 
 
 # @pytest.fixture(scope="module")
