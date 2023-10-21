f02d47a4a9d66585ed868459fde2c5bb00adfa79
Fidail Shafikov <93327801+Shafiddam@users.noreply.github.com>
2023-10-21 14:29:49 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import pytest
 from selenium.webdriver.chrome.options import Options  # для запуска браузера в безоконном режиме; на английском
 from selenium.webdriver.chrome.service import Service
-from pages.login_page import LoginPage
+from .pages.login_page import LoginPage
 from pages.dashboard_page import DashboardPage
 from data.data import *
 import os
