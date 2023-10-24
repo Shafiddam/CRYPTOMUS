12aad11fad12461aa5bc256521d7bb4ff35a6b36
Fidail Shafikov <93327801+Shafiddam@users.noreply.github.com>
2023-10-24 10:40:33 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -13,7 +13,10 @@ import os
 def driver():
     chrome_options = Options()
     chrome_options.add_argument("--lang=en-US")  # Установите желаемый язык, например, en-US для английского
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--log-level=DEBUG")
     service = Service("/usr/local/bin/chromedriver")
     driver = webdriver.Chrome(service=service, options=chrome_options)
